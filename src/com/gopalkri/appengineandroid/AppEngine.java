6a0022779011ca4d0bb18928b88ea969278237f2
Gopal Sharma <gopalkrishnaps@gmail.com>
2010-12-27 00:07:16 -0800

    Added AppEngine class.

---
+++ src/com/gopalkri/appengineandroid/AppEngine.java
@@ -0,0 +1,13 @@
+/**
+ * This file is part of the AppEngineAndroid project.
+ * 
+ * Copyright (c) Gopal Sharma 2010.
+ */
+package com.gopalkri.appengineandroid;
+
+/**
+ * @author Gopal Sharma.
+ *
+ */
+public class AppEngine {
+}
--- src/com/gopalkri/appengineandroid/test/Home.java
+++ src/com/gopalkri/appengineandroid/test/Home.java
@@ -1,8 +1,17 @@
+/**
+ * This file is part of the AppEngineAndroid project.
+ * 
+ * Copyright (c) Gopal Sharma 2010.
+ */
 package com.gopalkri.appengineandroid.test;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author Gopal Sharma.
+ *
+ */
 public class Home extends Activity {
     /** Called when the activity is first created. */
     @Override
